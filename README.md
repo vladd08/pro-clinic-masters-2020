f38135efc041ba1fbf2068f62400178cb985db2e
Vlad Orbulescu <vlad.orbulescu@opswat.com>
2020-03-09 20:30:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pro-Clinic-Masters-2020
+This is my masters degree final project. It consists of a full stack web application built upon Angular, Node.js, Express and Firebase, a desktop application written on Electron and an Arduino board controlled with C.
---
+++ pro-clinic-authenticator/authenticator-arduino/authenticator-arduino.ino
@@ -0,0 +1,43 @@
+#include <rdm630.h>
+
+unsigned long int previous = 0;
+int redLed = 7;
+int greenLed = 8;
+
+rdm630 rfid(3, 0);  //TX-pin of RDM630 connected to Arduino pin 2
+
+void setup() {
+  pinMode(redLed,OUTPUT);
+  pinMode(greenLed,OUTPUT);
+  Serial.begin(9600);
+  rfid.begin();
+  while(!Serial){
+    ;
+  }
+}
+
+void loop() {
+  byte data[6];
+  byte length;
+
+  if(Serial.available()) {
+    char c = (char)Serial.read();
+    if(c == 'C') {
+      digitalWrite(greenLed,HIGH);
+    } else if(c == 'D') {
+      digitalWrite(greenLed,LOW);
+    }
+  }
+   
+  if(rfid.available()){
+       rfid.getData(data,length);
+       digitalWrite(redLed, HIGH);
+       unsigned long result = 
+         ((unsigned long int)data[1]<<24) + 
+         ((unsigned long int)data[2]<<16) + 
+         ((unsigned long int)data[3]<<8) + 
+         data[4];    
+       Serial.print(result);
+       digitalWrite(redLed, LOW);
+    }
+}

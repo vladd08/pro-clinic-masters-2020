19e3fc7c1c1d2504e35c0aa13c0b59ab7cdef581
Vlad Orbulescu <vladprbulescu10@gmail.com>
2020-03-14 18:03:19 +0200

    Add helmet and cors

--- pro-clinic-backend/app.js
+++ pro-clinic-backend/app.js
@@ -2,6 +2,8 @@ var createError = require("http-errors");
 var express = require("express");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var helmet = require("helmet");
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
@@ -10,6 +12,8 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(helmet);
+app.use(cors);
 app.use(cookieParser());
 
 app.use("/", indexRouter);

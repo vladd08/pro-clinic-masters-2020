cac65070e872471ba28eea284322e4238e949e75
Vlad Orbulescu <vladprbulescu10@gmail.com>
2020-03-14 15:30:57 +0200

    Generate random password each availability period tick

--- pro-clinic-authenticator/authenticator/src/preload.js
+++ pro-clinic-authenticator/authenticator/src/preload.js
@@ -1,5 +1,11 @@
 const SerialPort = require("electron").remote.require("serialport");
 const { ipcRenderer } = require("electron");
 
+const connectSignalBuffer = Buffer.from([0x43]);
+const disconnectSignalBuffer = Buffer.from([0x44]);
+
 window.SerialPort = SerialPort;
 window.ipcRenderer = ipcRenderer;
+
+window.connectSignalBuffer = connectSignalBuffer;
+window.disconnectSignalBuffer = disconnectSignalBuffer;
--- pro-clinic-authenticator/authenticator/src/renderer.js
+++ pro-clinic-authenticator/authenticator/src/renderer.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@material/elevation/dist/mdc.elevation.min.css";
@@ -6,6 +8,7 @@ import $ from "jquery";
 
 const SerialPort = window.SerialPort;
 const ipcRenderer = window.ipcRenderer;
+
 const portPath = "COM4";
 const baudRate = 9600;
 let port;
@@ -14,7 +17,7 @@ const connectToDevice = () => {
   $("#logo-icon").css("display", "none");
   $("#search-loader").css("display", "block");
   $("#connection-error").css("display", "none");
-        
+
   setTimeout(() => {
     port = new SerialPort(
       portPath,
@@ -23,34 +26,113 @@ const connectToDevice = () => {
       },
       err => {
         if (err) {
-          console.error(err);
+          console.error(err.message);
           $("#connection-error").css("display", "block");
+          $("#search-loader").css("display", "none");
+          $("#logo-icon").css("display", "block");
+          return;
         }
 
-        $("#search-loader").css("display", "none");
-        $("#logo-icon").css("display", "block");
+        sendConnectSignal();
       }
     );
   }, 2000);
 };
 
-setTimeout(() => {
-  $("#loading-spinner").fadeIn();
-
+const searchForDevice = () => {
   setTimeout(() => {
-    $("#loading-spinner").fadeOut({
-      complete: () => {
-        $("#main").show();
-        connectToDevice();
+    $("#loading-spinner").fadeIn();
+
+    setTimeout(() => {
+      $("#loading-spinner").fadeOut({
+        complete: () => {
+          $("#main").show();
+          connectToDevice();
+        }
+      });
+    }, 2500);
+  }, 300);
+};
+
+const handleAppClose = () => {
+  $("#close-btn").on("click", () => {
+    sendDisconnectSignal();
+    setTimeout(() => {
+      ipcRenderer.send("app-close");
+    }, 200);
+  });
+};
+
+const handleTryAgain = () => {
+  $("#try-again").on("click", () => {
+    connectToDevice();
+  });
+};
+
+const sendConnectSignal = () => {
+  setTimeout(() => {
+    port.write(window.connectSignalBuffer, err => {
+      if (err) {
+        console.error(err.message);
+        return;
       }
+
+      console.log(`Connected to port ${portPath} with baud rate ${baudRate}`);
+      $("#search-loader").css("display", "none");
+      $("#logo-icon").css("display", "block");
+      handlePortRead();
     });
-  }, 2500);
-}, 300);
+  }, 2000);
+};
 
-$("#close-btn").on("click", () => {
-  ipcRenderer.send("app-close");
-});
+const sendDisconnectSignal = () => {
+  port.write(window.disconnectSignalBuffer, err => {
+    if (err) {
+      console.error(err.message);
+      return;
+    }
+  });
+};
 
-$("#try-again").on("click", () => {
-  connectToDevice();
-});
+const handlePortRead = () => {
+  port.on("data", data => {
+    $("#rfid-code").val(data.toString());
+    $("#submit-btn").removeAttr("disabled");
+  });
+};
+
+const startGenerateInterval = () => {
+  let availablePeriodMs = 30000;
+  $("#submit-btn").attr("disabled", true);
+
+  setInterval(() => {
+    if (availablePeriodMs - 10 < 0) {
+      const password = Math.random()
+        .toString(36)
+        .substring(7)
+        .toUpperCase();
+
+      $("#password-field").html(password);
+      availablePeriodMs = 30000;
+    }
+
+    availablePeriodMs -= 10;
+
+    const percent = (availablePeriodMs * 100) / 30000;
+    $("#availability-bar").width(percent + "%");
+  }, 1);
+};
+
+const handleSubmit = () => {
+  $("#submit-btn").on("click", () => {
+    $("#password-container")
+      .css("display", "flex")
+      .show();
+    startGenerateInterval();
+  });
+};
+
+searchForDevice();
+handleAppClose();
+handleTryAgain();
+handleSubmit();

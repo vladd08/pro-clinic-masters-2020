6f675e84577ae032ad6bf4757ffb0bbc6073ee91
Vlad Orbulescu <vlad.orbulescu@opswat.com>
2020-04-11 13:53:53 +0300

    Add initial client code

--- pro-clinic-client/src/app/app.module.ts
+++ pro-clinic-client/src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { CoreModule } from 'src/core/core.module'
+import { SharedModule } from 'src/shared/shared.module'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [CoreModule, SharedModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ pro-clinic-client/src/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'src/shared/shared.module';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    BrowserAnimationsModule
+  ]
+})
+export class CoreModule { }
